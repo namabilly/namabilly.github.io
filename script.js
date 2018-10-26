2a0f84b70bf23e76eac7174a2962d0b25503526d
0Accelerator0 <948386361@qq.com>
2018-10-26 13:22:36 +0800

    Implemented dropdown, carousel

---
+++ script.js
@@ -0,0 +1,57 @@
+var main = function(){
+    
+    
+    $('.arrow-next').click(function() {
+        slidenext();
+    });
+    $('.arrow-prev').click(function() {
+        slideprev();    
+    });
+    
+    carousel();
+};
+
+function carousel() {
+    slidenext();
+    setTimeout(carousel, 7000); // Change image every 7 seconds
+}
+
+var slidenext = function(){
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+};
+
+var slideprev = function(){
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+};
+
+$(document).ready(main);
